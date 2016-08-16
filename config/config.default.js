7702197ea5a77d16d0a87541e17eb533a2721e86
popomore <sakura9515@gmail.com>
2016-08-17 01:22:58 +0800

    feat: rename logrotater => logrotator

--- config/config.default.js
+++ config/config.default.js
@@ -1,15 +1,15 @@
 'use strict';
 
 /**
- * logrotater options
- * @member Config#logrotater
+ * logrotator options
+ * @member Config#logrotator
  * @property {Array} filesRotateBySize - Array for files path which need rotate
  * @property {Number} maxFileSize - Max file size to judge if any file need rotate
  * @property {Number} maxFiles - pieces rotate by size
  * @property {Number} maxDays - keep max days log files, default is `31`. Set `0` to keep all logs.
  * @property {Number} rotateDuration - time interval to judge if any file need rotate
  */
-exports.logrotater = {
+exports.logrotator = {
   filesRotateBySize: [],
   maxFileSize: 50 * 1024 * 1024,
   maxFiles: 10,
