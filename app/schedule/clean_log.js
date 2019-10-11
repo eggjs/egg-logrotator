46b68764756281af6beb52ea47730b2090b12b1e
TZ | 天猪 <atian25@qq.com>
2019-10-11 10:33:09 +0800

    chore: typo (#27)

--- app/schedule/clean_log.js
+++ app/schedule/clean_log.js
@@ -5,7 +5,7 @@ const fs = require('mz/fs');
 const moment = require('moment');
 const utils = require('../../utils');
 
-// clean all xxx.log.YYYY-MM-DD beofre expried date.
+// clean all xxx.log.YYYY-MM-DD before expired date.
 module.exports = app => ({
   schedule: {
     type: 'worker', // only one worker run this task
@@ -41,7 +41,7 @@ async function removeExpiredLogFiles(logdir, maxDays, logger) {
   if (!exists) return;
 
   const files = await fs.readdir(logdir);
-  const expriedDate = moment().subtract(maxDays, 'days').startOf('date');
+  const expiredDate = moment().subtract(maxDays, 'days').startOf('date');
   const names = files.filter(file => {
     const name = path.extname(file).substring(1);
     if (!/^\d{4}\-\d{2}\-\d{2}/.test(name)) {
@@ -51,7 +51,7 @@ async function removeExpiredLogFiles(logdir, maxDays, logger) {
     if (!date.isValid()) {
       return false;
     }
-    return date.isBefore(expriedDate);
+    return date.isBefore(expiredDate);
   });
   if (names.length === 0) {
     return;
